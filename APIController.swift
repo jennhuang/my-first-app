ab8042ad9ec517a943b7f114ff983c4ec00820d3
jennhuang8@gmail.com <jennifer@Jennifers-MacBook-Pro.local>
2014-08-26 23:16:25 -0700

    part 3

---
+++ APIController.swift
@@ -0,0 +1,9 @@
+//
+//  APIController.swift
+//  HelloWorld
+//
+//  Created by Jennifer Huang on 8/26/14.
+//  Copyright (c) 2014 Jennifer Huang. All rights reserved.
+//
+
+import Foundation
---
+++ HelloWorld/APIController.swift
@@ -0,0 +1,50 @@
+//
+//  APIController.swift
+//  HelloWorld
+//
+//  Created by Jennifer Huang on 8/26/14.
+//  Copyright (c) 2014 Jennifer Huang. All rights reserved.
+//
+
+import Foundation
+
+protocol APIControllerProtocol {
+    func didReceiveAPIResults(results: NSDictionary)
+}
+
+class APIController {
+    var delegate: APIControllerProtocol?
+    
+    init() {
+        
+    }
+    
+    func searchItunesFor(searchTerm: String) {
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range:nil)
+        
+        // escape anything that is not URL friendly
+        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let url: NSURL = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+                if(error != nil) {
+                    // error in web request, print to console
+                    println(error.localizedDescription)
+                }
+                var err: NSError?
+                
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                if(err != nil) {
+                    // error parsing JSON
+                    println("JSON Error \(err!.localizedDescription)")
+                }
+                let results: NSArray = jsonResult["results"] as NSArray
+                self.delegate?.didReceiveAPIResults(jsonResult)
+            })
+            task.resume()
+        }
+    }
+
+}
