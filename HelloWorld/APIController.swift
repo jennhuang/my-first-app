0e2f5216fa5fef129122d0f6162ebe8a83fc05f9
Jennifer Huang <jennhuang8@gmail.com>
2014-09-01 21:06:40 -0700

    part 6 in progress

--- HelloWorld/APIController.swift
+++ HelloWorld/APIController.swift
@@ -13,10 +13,10 @@ protocol APIControllerProtocol {
 }
 
 class APIController {
-    var delegate: APIControllerProtocol?
+    var delegate: APIControllerProtocol
     
-    init() {
-        
+    init(delegate: APIControllerProtocol) {
+        self.delegate = delegate
     }
     
     func searchItunesFor(searchTerm: String) {
@@ -25,7 +25,7 @@ class APIController {
         
         // escape anything that is not URL friendly
         if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
-            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=music&entity=album"
             let url: NSURL = NSURL(string: urlPath)
             let session = NSURLSession.sharedSession()
             let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
@@ -41,7 +41,7 @@ class APIController {
                     println("JSON Error \(err!.localizedDescription)")
                 }
                 let results: NSArray = jsonResult["results"] as NSArray
-                self.delegate?.didReceiveAPIResults(jsonResult)
+                self.delegate.didReceiveAPIResults(jsonResult)
             })
             task.resume()
         }
--- HelloWorld/SearchResultsViewController.swift
+++ HelloWorld/SearchResultsViewController.swift
@@ -13,24 +13,26 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
     let kCellIdentifier: String = "SearchResultCell"
 
     @IBOutlet var appsTableView : UITableView?
-    var api = APIController()
-    var tableData = []
+    var api : APIController?
+    var albums = [Album]()
     var imageCache = [String : UIImage]()
     
     
     func didReceiveAPIResults(results: NSDictionary) {
         var resultsArr: NSArray = results["results"] as NSArray
         dispatch_async(dispatch_get_main_queue(), {
-            self.tableData = resultsArr
+            self.albums = Album.albumsWithJSON(resultsArr)
             self.appsTableView!.reloadData()
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
         })
     }
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        api.searchItunesFor("Salesforce")
-        self.api.delegate = self
+        api = APIController(delegate: self)
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        api!.searchItunesFor("Frozen")
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,27 +41,23 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
     }
     
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        return tableData.count
+        return albums.count
     }
 
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
         
-        // get the row data for the selected row
-        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
-        
-        // Add a check to make sure this exists
-        let cellText: String? = rowData["trackName"] as? String
-        cell.textLabel.text = cellText
+        let album = self.albums[indexPath.row]
+        cell.textLabel.text = album.title
         cell.imageView.image = UIImage(named: "Blank52")
         
         // Get the formatted price string for display in the subtitle
-        let formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        let formattedPrice = album.price
         
         // Jump in to a background thread to get the image for this item
         
         // Grab the artworkUrl60 key to get an image url for the app's thumbnail
-        let urlString = rowData["artworkUrl60"] as String
+        let urlString = album.thumbnailImageURL
         
         // Check our image cache for the existing key. This is just a dictionary of UIImages
         // var image: UIImage? = self.imageCache.valueForKey(urlString) as? UIImage
@@ -95,20 +93,6 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
         }
         cell.detailTextLabel.text = formattedPrice
         return cell
-    }
-    
-    func tableView(tableView: UITableView!, didDeselectRowAtIndexPath indexPath: NSIndexPath!) {
-        // get the row data for the selected row
-        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
-        
-        var name: String = rowData["trackName"] as String
-        var formattedPrice: String = rowData["formattedPrice"] as String
-        
-        var alert: UIAlertView = UIAlertView()
-        alert.title = name
-        alert.message = formattedPrice
-        alert.addButtonWithTitle("Ok")
-        alert.show()
     }
    
 }
