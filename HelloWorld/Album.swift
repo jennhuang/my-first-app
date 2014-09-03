b5c438200175134316288223f313027e2806aeab
Jennifer Huang <jennhuang8@gmail.com>
2014-09-02 23:07:06 -0700

    part 6 in-progress-part2

--- HelloWorld/Album.swift
+++ HelloWorld/Album.swift
@@ -15,6 +15,7 @@ class Album {
     var largeImageURL: String
     var itemURL: String
     var artistURL: String
+    
     init(name: String, price: String, thumbnailImageURL: String, largeImageURL: String, itemURL: String, artistURL: String) {
         self.title = name
         self.price = price
@@ -24,48 +25,48 @@ class Album {
         self.artistURL = artistURL
     }
     
-}
-
-class func albumsWithJSON(allResults: NSArray) -> [Album] {
-    // create an empty array of Albums to append to from this list
-    var albums = [Album]()
-    
-    //store the results in our table data array
-    if allResults.count>0 {
+    class func albumsWithJSON(allResults: NSArray) -> [Album] {
+        // create an empty array of Albums to append to from this list
+        var albums = [Album]()
         
-        // sometimes iTunes returns a collection, not a track so we check both for the 'name'
-        for result in allResults {
+        //store the results in our table data array
+        if allResults.count>0 {
             
-            var name = result["trackName"] as? String
-            if name == nil {
-                name = result["collectionName"] as? String
-            }
-            
-            // sometimes price comes as formattedPrice, sometimes as collectionPrice and sometimes  it's a float instead of a string
-            var price = result["formattedPrice"] as? String
-            if price == nil {
-                price = result["collectPrice"] as? String
+            // sometimes iTunes returns a collection, not a track so we check both for the 'name'
+            for result in allResults {
+                
+                var name = result["trackName"] as? String
+                if name == nil {
+                    name = result["collectionName"] as? String
+                }
+                
+                // sometimes price comes as formattedPrice, sometimes as collectionPrice and sometimes  it's a float instead of a string
+                var price = result["formattedPrice"] as? String
                 if price == nil {
-                    var priceFloat: Float? = result["collectionPrice"] as? Float
-                    var nf: NSNumberFormatter = NSNumberFormatter()
-                    nf.maximumFractionDigits = 2
-                    if priceFloat != nil {
-                        price = "$"+nf.stringFromNumber(priceFloat)
+                    price = result["collectPrice"] as? String
+                    if price == nil {
+                        var priceFloat: Float? = result["collectionPrice"] as? Float
+                        var nf: NSNumberFormatter = NSNumberFormatter()
+                        nf.maximumFractionDigits = 2
+                        if priceFloat != nil {
+                            price = "$"+nf.stringFromNumber(priceFloat)
+                        }
                     }
                 }
+                let thumbnailURL = result["artworkUrl60"] as? String ?? ""
+                let imageURL = result["artworkUrl100"] as? String ?? ""
+                let artistURL = result["artistViewurl"] as? String ?? ""
+                
+                var itemURL = result["collectionViewUrl"] as? String
+                if itemURL == nil {
+                    itemURL = result["trackViewUrl"] as? String
+                }
+                
+                var newAlbum = Album(name: name!, price: price!, thumbnailImageURL: thumbnailURL, largeImageURL: imageURL, itemURL: itemURL!, artistURL: artistURL)
+                albums.append(newAlbum)
             }
-            let thumbnailURL = result["artworkUrl60"] as? String ?? ""
-            let imageURL = result["artworkUrl100"] as? String ?? ""
-            let artistURL = result["artistViewurl"] as? String ?? ""
-            
-            var itemURL = result["collectionViewUrl"] as? String
-            if itemURL == nil {
-                itemURL = result["trackViewUrl"] as? String
-            }
-            
-            var newAlbum = Album(name: name!, price: price!, thumbnailImageURL: thumbnailURL, largeImageURL: imageURL, itemURL: itemURL!, artistURL: artistURL)
-            albums.append(newAlbum)
         }
+        return albums
     }
-    return albums
 }
+
---
+++ HelloWorld/DetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailsViewController.swift
+//  HelloWorld
+//
+//  Created by Jennifer Huang on 9/2/14.
+//  Copyright (c) 2014 Jennifer Huang. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    var album: Album?
+    
+    required init(code aDecoder:NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+    
